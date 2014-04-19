b829e9c121c4c7ab9b91ef126e3682c414ae99b4
jetpacksWasYes <gdoorenbos@yahoo.com>
2014-04-18 19:24:44 -0500

    Added libraries, ino moved to folder

---
+++ libraries/Timer2560/Timer2560.c
@@ -0,0 +1,7 @@
+#include "Timer2560.h"
+
+Timer2560::Timer2560()
+{
+	// default constructor
+	
+}
---
+++ libraries/Timer2560/Timer2560.h
@@ -0,0 +1,13 @@
+#ifndef Timer2560_h
+#define Timer2560_h
+
+class Timer2560
+{
+	public:
+	Timer2560();
+	
+	private:
+
+};
+
+#endif
